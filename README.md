52059f2797d9555d23d8a52f5dc8eaca575d87f2
Vladimir Degtev <mr.dolte@gmail.com>
2024-08-27 20:33:45 +0300

    Update

--- README.md
+++ README.md
@@ -35,40 +35,44 @@ To use the Pin Code Plugin, follow these steps:
 1. Include the Pin Code Plugin script in your HTML file by adding the following line in the `<head>` section:
 
    ```html
-   <script src="https://cdn.jsdelivr.net/gh/degtev/pin-code-js-plugin@main/src/pincode-plugin.min.js"></script>
+      <script src="/src/pincode-plugin.js"></script>
    ```
-   Or
-   ```html
-   <script src="https://cdn.jsdelivr.net/gh/degtev/pin-code-js-plugin@main/src/pincode-plugin.js"></script>
-   ```
-   
+
    Alternatively, you can download the `pincode-plugin.js` file from the [GitHub repository](https://github.com/degtev/pin-code-js-plugin) and include it manually in your project.
 
 2. Add the required HTML structure for the pin code input fields:
 
-   ```html
+    ```html
    <div id="pinCodeContainer">
      <input type="text" maxlength="1" />
      <input type="text" maxlength="1" />
      <input type="text" maxlength="1" />
      <input type="text" maxlength="1" />
    </div>
+   <button id="buttonClick">Click!</button>
    ```
 
-   Make sure to set the `maxlength` attribute to the desired length of your pin code.
+   Make sure to set the `maxlength` attribute to `1` for each input field.
 
 3. Initialize the plugin with the container element:
 
-   ```javascript
-   PinCodePlugin.init("#pinCodeContainer");
-   ```
-
+```html
+<script>
+    function onSuccess(pinCode) {
+       alert("Your pin code: " + pinCode);
+    }
+    function onError(pinCode) {
+       alert("Please enter the full pin code.");
+    }
+    PinCodePlugin.init('.pin-code', onSuccess, onError, 'buttonClick');
+</script>
+```
 ## Usage
 
 To use the Pin Code Plugin, follow these steps:
 
 1. Create a container element that will hold the pin code input fields.
-2. Inside the container, add individual input fields to represent each digit of the pin code. Set the `maxlength` attribute on each input field to specify the length of the pin code.
+2. Inside the container, add individual input fields to represent each digit of the pin code. Set the `maxlength` attribute on each input field to `1`.
 3. Initialize the plugin by calling the `init()` method and passing the selector for the container element as an argument.
 4. Customize the plugin behavior and appearance by modifying the provided methods and CSS styles.
 
@@ -76,7 +80,7 @@ To use the Pin Code Plugin, follow these steps:
 
 The Pin Code Plugin provides the following methods:
 
-- `init(selector)`: Initializes the plugin by finding the pin code container element and registering event handlers.
+- `init(selector, onSuccess, onError, buttonId)`: Initializes the plugin by finding the pin code container element, registering event handlers, and optionally linking to a button for manual submission.
 
 ## Events
 
@@ -85,10 +89,11 @@ The Pin Code Plugin triggers the following events:
 - `input`: Fired when an input field value changes.
 - `paste`: Fired when text is pasted into the container element.
 - `keydown`: Fired when a key is pressed down inside an input field.
+- `keyup`: Fired when a key is released inside an input field.
 
 ## Demo
 
-Check out the live demo of the Pin Code Plugin [here](https://plugin-pincode.cloud24.space/). 🌐🚀
+Check out the live demo of the Pin Code Plugin [here](https://degtev.github.io/pin-code-js-plugin/). 🌐🚀
 
 ## Contributing
 
--- src/pincode-plugin.js
+++ src/pincode-plugin.js
@@ -3,31 +3,45 @@
  * Author: Vladimir Degtev
  * Created: December 26, 2023
  * GitHub: https://github.com/degtev/pin-code-js-plugin
- * Version: 1.0.1
+ * Version: 1.5.0
  */
 
 const PinCodePlugin = {
     pinContainer: null,
     pinInputs: [],
     pinInputsLen: 0,
+    onSuccess: null, // Function that will be called after entering a correct pin code
+    onError: null,   // Function that will be called if the pin code is not fully entered
+    button: null,    // Button to perform the function on click
 
     // Plugin initialization
-    init: function(selector) {
+    init: function(selector, onSuccess = null, onError = null, buttonId = null) {
         this.pinContainer = document.querySelector(selector);
         this.pinInputs = Array.from(this.pinContainer.getElementsByTagName("input"));
         this.pinInputsLen = this.pinInputs.length;
+        this.onSuccess = onSuccess;
+        this.onError = onError;
 
-        this.bindEvents(); // Binds event handlers
+        // If buttonId is provided, find the button by ID
+        if (buttonId) {
+            this.button = document.getElementById(buttonId);
+            if (this.button) {
+                this.button.addEventListener('click', this.handleButtonClick.bind(this));
+            }
+        }
+
+        this.bindEvents(); // Bind event handlers
     },
 
-    // Binds event handlers
+    // Bind event handlers
     bindEvents: function() {
         this.pinContainer.addEventListener('input', this.handleInput.bind(this), false);
         this.pinContainer.addEventListener('paste', this.handlePaste.bind(this), false);
         this.pinContainer.addEventListener('keydown', this.handleKeyDown.bind(this), false);
+        this.pinContainer.addEventListener('keyup', this.handleKeyUp.bind(this), false);
     },
 
-    // Focuses on the next input field
+    // Focus on the next input field
     focusNextInput: function(currentInput) {
         const nextInput = currentInput.nextElementSibling;
         if (nextInput && nextInput.tagName.toLowerCase() === "input") {
@@ -35,31 +49,41 @@ const PinCodePlugin = {
         }
     },
 
-    // Handles the input event
-    handleInput: function(event) {
-        const target = event.target;
-        const maxLength = parseInt(target.getAttribute("maxlength"), 10);
-        const myLength = target.value.length;
-
-        if (myLength === this.pinInputsLen) {
-            const pastedData = target.value;
-            const numericPattern = /\d/g;
-            const pinCode = pastedData.match(numericPattern).join('');
-
-            this.pinInputs.forEach((input) => {
-                input.value = "";
-            });
-
-            for (let i = 0; i < pinCode.length && i < this.pinInputsLen; i++) {
-                const index = i % this.pinInputs.length;
-                this.pinInputs[index].value = pinCode[i];
-            }
+    // Focus on the previous input field
+    focusPreviousInput: function(currentInput) {
+        const previousInput = currentInput.previousElementSibling;
+        if (previousInput && previousInput.tagName.toLowerCase() === "input") {
+            previousInput.focus();
         }
-
-        this.focusNextInput(target);
     },
 
-    // Handles the paste event
+    // Handle input event
+    handleInput: function(event) {
+        const target = event.target;
+        const value = target.value;
+
+        // Allow only one digit per input field
+        if (value.length > 1) {
+            target.value = value.charAt(0);
+        }
+
+        // Check if the entered value is a digit
+        if (!/^\d$/.test(target.value)) {
+            target.value = ""; // Clear the field if a non-digit is entered
+            return;
+        }
+
+        const currentIndex = this.pinInputs.indexOf(target);
+
+        if (currentIndex === this.pinInputsLen - 1) {
+            // If all fields are filled, call the onSuccess function on key release
+            this.checkPinCodeCompletion();
+        } else {
+            this.focusNextInput(target);
+        }
+    },
+
+    // Handle paste event
     handlePaste: function(event) {
         event.preventDefault();
         const pastedData = event.clipboardData.getData('text/plain');
@@ -71,10 +95,12 @@ const PinCodePlugin = {
             this.pinInputs[index].value = pinCode[i];
         }
 
-        this.focusNextInput(this.pinInputs[this.pinInputsLen - 1]);
+        if (pinCode.length >= this.pinInputsLen) {
+            this.focusNextInput(this.pinInputs[this.pinInputsLen - 1]);
+        }
     },
 
-    // Handles the keydown event
+    // Handle key down event
     handleKeyDown: function(event) {
         const target = event.target;
         const isBackspace = event.key === 'Backspace';
@@ -84,5 +110,47 @@ const PinCodePlugin = {
             previousInput.focus();
             previousInput.value = "";
         }
+    },
+
+    // Handle key up event
+    handleKeyUp: function(event) {
+        this.checkPinCodeCompletion();
+    },
+
+    // Check if the pin code input is complete
+    checkPinCodeCompletion: function() {
+        const pinCode = this.getPinCode();
+        if (pinCode.length === this.pinInputsLen && this.onSuccess) {
+            this.onSuccess(pinCode);
+        }
+    },
+
+    // Get the current entered PIN code
+    getPinCode: function() {
+        return this.pinInputs.map(input => input.value).join('');
+    },
+
+    // Handle button click
+    handleButtonClick: function() {
+        const pinCode = this.getPinCode();
+        if (pinCode.length === this.pinInputsLen) {
+            if (this.onSuccess) {
+                this.onSuccess(pinCode);
+            }
+        } else {
+            if (this.onError) {
+                this.onError(pinCode);
+            }
+        }
     }
 };
+// Example usage:
+// Define functions
+//function onSuccess(pinCode) {
+//    alert("Your pin code: " + pinCode);
+//}
+//function onError(pinCode) {
+//    alert("Please enter the full pin code.");
+//}
+// Initialize PinCodePlugin with onSuccess and onError functions
+//PinCodePlugin.init('.pin-code', onSuccess, onError, 'buttonClick');
